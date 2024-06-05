5c5d3459cb07259bd59de122c768221347a7c27a
Andre Demavivas <andredemavivas@gmail.com>
2024-06-05 16:24:41 +0800

    initial commit

--- components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,15 +1,29 @@
 'use client';
 
 import { Button, Group, useMantineColorScheme } from '@mantine/core';
+import { useRouter } from 'next/navigation';
+import styled from 'styled-components';
 
 export function ColorSchemeToggle() {
+  const router = useRouter();
   const { setColorScheme } = useMantineColorScheme();
 
   return (
-    <Group justify="center" mt="xl">
-      <Button onClick={() => setColorScheme('light')}>Light</Button>
-      <Button onClick={() => setColorScheme('dark')}>Dark</Button>
-      <Button onClick={() => setColorScheme('auto')}>Auto</Button>
-    </Group>
+    <>
+      <Group justify="center" mt="xl">
+        <Button onClick={() => setColorScheme('light')}>Light</Button>
+        <Button onClick={() => setColorScheme('dark')}>Dark</Button>
+        <Button onClick={() => setColorScheme('auto')}>Auto</Button>
+      </Group>
+      <LoginButtonWrapper>
+        <Button onClick={() => router.push('/login')}>Login</Button>
+      </LoginButtonWrapper>
+    </>
   );
 }
+
+const LoginButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+`;
